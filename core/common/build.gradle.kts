376f85585cbcb6bf71c4ce827d861f85c7e6015e
Haitham Altamemi <92040536+Haithamaltamemi@users.noreply.github.com>
2026-06-06 07:15:02 +0300

    Add core/common module

---
+++ core/common/build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    id("com.android.library")
+    kotlin("android")
+}
+
+android {
+    namespace = "com.baran.calendar.core.common"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 26
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.12.0")
+}
