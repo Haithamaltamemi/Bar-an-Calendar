1f86024130c3a38e2bdc6dfed1e371d38247123c
Haitham Altamemi <92040536+Haithamaltamemi@users.noreply.github.com>
2026-06-06 08:26:32 +0300

    Create build.gradle.kts

---
+++ domain/sabaean/build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    id("com.android.library")
+    kotlin("android")
+}
+
+android {
+    namespace = "com.baran.calendar.domain.sabaean"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 26
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+dependencies {
+    implementation(project(":core:common"))
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+}
